6821d8c2bb879735ac31f044fd30709ebd462897
solibe1 <beshoysoliman11@gmail.com>
2021-10-29 17:15:15 -0400

    adding test class  for Pet class

--- src/test/java/org/springframework/samples/petclinic/owner/PetTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTests.java
@@ -1,4 +1,26 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.samples.petclinic.visit.Visit;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PetTests {
+
+    @Test
+    void testPet(){
+        Pet pet = new Pet();
+        Owner owner = Mockito.mock(Owner.class);
+        Visit visit = Mockito.mock(Visit.class);
+        PetType type = Mockito.mock(PetType.class);
+        pet.setOwner(owner);
+
+    }
+
+
+
+
+
 }
