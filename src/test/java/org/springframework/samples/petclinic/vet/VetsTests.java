b2aedc300e57d87705027e88ceb5994da9e08692
solibe1 <beshoysoliman11@gmail.com>
2021-10-31 12:24:03 -0400

    adding test for Vets class

--- src/test/java/org/springframework/samples/petclinic/vet/VetsTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetsTests.java
@@ -1,13 +1,34 @@
 package org.springframework.samples.petclinic.vet;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class VetsTests {
      @Test
 
-    void VetsTest(){
+    void TestGetVetList(){
 
          Vets vets = new Vets();
-         vets.getVetList();
+         Vet vet1 = Mockito.mock(Vet.class);
+         Vet vet2 = Mockito.mock(Vet.class);
+         Specialty specialty = Mockito.mock(Specialty.class);
+         Specialty specialty2 = Mockito.mock(Specialty.class);
+
+         when(vet1.getFirstName()).thenReturn("James");
+         when(vet2.getFirstName()).thenReturn("Helen");
+         when(specialty.getName()).thenReturn("radiology");
+         when(specialty2.getName()).thenReturn("surgery");
+         vet1.addSpecialty(specialty);
+         vet2.addSpecialty(specialty2);
+         vets.getVetList().add(vet1);
+         vets.getVetList().add(vet2);
+         assertEquals(vet1,vets.getVetList().get(0));
+         assertEquals(vet2,vets.getVetList().get(1));
+
+
+
      }
 }
