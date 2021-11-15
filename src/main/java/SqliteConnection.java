0d442c57924598e1d6b71ea3106321727fd5b768
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2021-11-15 10:13:38 -0500

    SQLite connection successful

---
+++ src/main/java/SqliteConnection.java
@@ -0,0 +1,56 @@
+import java.sql.*;
+
+public class SqliteConnection {
+
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:identifier.sqlite";
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite successful");
+
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void query(String query) {
+        Connection conn = connect();
+        if (conn != null) {
+            try {
+                Statement statement = conn.createStatement();
+                ResultSet queryResult = statement.executeQuery(query);
+
+                while (queryResult.next()) {
+                    System.out.println(queryResult.getInt("id") + "\t" +
+                            queryResult.getString("first_name") + "\t" +
+                            queryResult.getString("last_name"));
+                }
+            }
+            catch(SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            finally {
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                }
+                catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        query("INSERT INTO vets (id, first_name, last_name ) VALUES (1, 'James', 'Carter')");
+        query("SELECT id, first_name, last_name FROM vets");
+    }
+}
