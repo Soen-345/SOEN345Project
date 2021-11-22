aa73099765f2534f3e7dd99c5a46b87afea48754
alireza ziarizi <alireza.ziaeework@gmail.com>
2021-11-21 19:56:11 -0500

    forklift for owner migration

---
+++ src/test/java/org/springframework/samples/petclinic/migration/OwnerMigrationTest.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.migration;
+
+import org.junit.jupiter.api.Test;
+
+public class OwnerMigrationTest {
+
+    @Test
+    public void forklift(){
+
+    }
+}
