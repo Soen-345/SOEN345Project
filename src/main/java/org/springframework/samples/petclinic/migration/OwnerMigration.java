9482c9dd1b1780d95ecf3e913a0c28a0c268bb9d
alireza ziarizi <alireza.ziaeework@gmail.com>
2021-11-21 21:37:15 -0500

    forklift method, unit testing for forklift method done

--- src/main/java/org/springframework/samples/petclinic/migration/OwnerMigration.java
+++ src/main/java/org/springframework/samples/petclinic/migration/OwnerMigration.java
@@ -3,15 +3,46 @@ package org.springframework.samples.petclinic.migration;
 import org.springframework.samples.petclinic.owner.Owner;
 
 
+import javax.xml.crypto.Data;
 import java.util.Map;
-
+/**
+ * @author Alireza Ziarizi
+ */
 public class OwnerMigration {
 
     private void initTable() {
+        String deleteQuery = "DROP TABLE IF EXISTS vets";
+        QueryController.queryVets(deleteQuery, Datastore.SQLITE, false);
 
+        String createQuery = "CREATE TABLE onwers (\n" +
+                "                      id         INTEGER IDENTITY PRIMARY KEY,\n" +
+                "                      first_name VARCHAR(30),\n" +
+                "                      last_name VARCHAR(30),\n" +
+                "                      address VARCHAR(255),\n" +
+                "                      city VARCHAR(80),\n" +
+                "                      telephone VARCHAR(20),\n" +
+                ");\n" +
+                "CREATE INDEX vets_last_name ON vets (last_name);";
+        QueryController.queryVets(createQuery, Datastore.SQLITE, false);
     }
 
     public void forklift(Map<Integer, Owner> owners) {
+        if(!DatastoreToggles.isUnderTest){
+            String selectQuery = "SELECT * FROM owners";
+            owners = QueryController.queryOwners(selectQuery,Datastore.H2,true);
+        }
+
+        this.initTable();
+        for (Owner owner : owners.values()){
+            String insterQuery = "INSERT INTO owners (id, first_name, last_name, address, city, telephone ) VALUES ('"
+                    + owner.getId() +  "','"
+                    + owner.getFirstName() +  "','"
+                    + owner.getLastName() +  "','"
+                    + owner.getAddress() +  "','"
+                    + owner.getCity() +   "','"
+                    + owner.getTelephone() + "');";
+            QueryController.queryOwners(insterQuery,Datastore.SQLITE,false);
+        }
 
     }
 
--- src/test/java/org/springframework/samples/petclinic/migration/OwnerMigrationTest.java
+++ src/test/java/org/springframework/samples/petclinic/migration/OwnerMigrationTest.java
@@ -1,11 +1,70 @@
 package org.springframework.samples.petclinic.migration;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.samples.petclinic.owner.Owner;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class OwnerMigrationTest {
+    private static final String SQLite_URL_TEST = "jdbc:sqlite:test-pet-clinic";
+    private OwnerMigration ownerMigration;
+    private Connection testDbConnections;
+    private Map<Integer, Owner> oldDataStoreOwners;
+
+    @Mock
+    Owner owner1;
+    @Mock
+    Owner owner2;
+
+
+    @BeforeEach
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+        DatastoreToggles.isUnderTest = true;
+        ownerMigration = new OwnerMigration();
+        // owner 1 data
+        when(owner1.getId()).thenReturn(1);
+        when(owner1.getFirstName()).thenReturn("George");
+        when(owner1.getLastName()).thenReturn("Franklin");
+        when(owner1.getAddress()).thenReturn("110 W. Liberty St.");
+        when(owner1.getCity()).thenReturn("Madison");
+        when(owner1.getTelephone()).thenReturn("6085551023");
+
+        //owner 2 data
+        when(owner2.getId()).thenReturn(2);
+        when(owner2.getFirstName()).thenReturn("Betty");
+        when(owner2.getLastName()).thenReturn("Davis");
+        when(owner2.getAddress()).thenReturn("638 Cardinal Ave.");
+        when(owner2.getCity()).thenReturn("Sun Prairie");
+        when(owner2.getTelephone()).thenReturn("6085551749");
+
+        oldDataStoreOwners = new HashMap<>();
+
+        oldDataStoreOwners.put(owner1.getId(),owner1);
+        oldDataStoreOwners.put(owner2.getId(),owner2);
+    }
+
+
 
     @Test
-    public void forklift(){
+    public void testforklift() throws SQLException {
+        ownerMigration.forklift(oldDataStoreOwners);
+        testDbConnections = DriverManager.getConnection(SQLite_URL_TEST);
 
+        if(testDbConnections != null){
+            Statement statement = testDbConnections.createStatement();
+            assertTrue(statement.execute("SELECT * FROM owners"));
+        }
     }
 }
