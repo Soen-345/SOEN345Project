1f54816ec821d39e5a2962327443b5f6e2d4b44c
mikenkie <ineke.kim@gmail.com>
2021-11-24 13:56:48 -0500

    moved the toggle for the add owner button in the OwnerToggles.java file

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerToggles.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerToggles.java
@@ -1,6 +1,13 @@
 package org.springframework.samples.petclinic.owner;
 
 public class OwnerToggles {
+
+    // The following two booleans are for the new feature
     public static boolean isSearchFirstNameEnabled=true;
     public static boolean isSearchLastNameEnabled=false;
+
+
+    // toggle for existing feature
+    // this toggle is for the adding owner button feature which is located in the "Find Owner" tab of the webpage
+    public static Boolean isAddOwnerButtonEnabled = true;
 }
