b48aece5fe19196a96ce12fc23a8a8585e85dd89
mikenkie <ineke.kim@gmail.com>
2021-11-24 13:41:13 -0500

    toggle for old feature --> adding owner

---
+++ src/main/java/org/springframework/samples/petclinic/TogglesForExistingFeatures/TogglesForOldFeatures.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic.TogglesForExistingFeatures;
+
+public class TogglesForOldFeatures {
+
+    //this toggle is for the adding owner button feature which is located in the "Find Owner" tab of the webpage
+    public static Boolean isAddOwnerButtonOn = true;
+
+
+
+
+
+}
