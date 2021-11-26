0e28d1604afa70421b62a8e9e36fbcfdb0e57689
mikenkie <ineke.kim@gmail.com>
2021-11-26 13:49:42 -0500

    Logger test file.
    In addition, add the following to pom file:
           <dependency>
                <groupId>org.apache.logging.log4j</groupId>
                <artifactId>log4j-api</artifactId>
                <version>2.14.1</version>
            </dependency>
            <dependency>
                <groupId>org.apache.logging.log4j</groupId>
                <artifactId>log4j-core</artifactId>
                <version>2.14.1</version>
            </dependency>

---
+++ src/test/java/org/springframework/samples/petclinic/LoggingTest.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+
+
+public class LoggingTest {
+
+    private static Logger logger = LogManager.getLogger();
+    @Test
+    public void testLogger()
+    {
+
+    }
+}
