1affb9aba8fdf306428d3ee9eab9ce77a0fb9007
mikenkie <ineke.kim@gmail.com>
2021-11-26 14:17:30 -0500

    moved logger test in OwnerController

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -30,6 +30,9 @@ import org.springframework.samples.petclinic.visit.Visit;
 import org.springframework.samples.petclinic.visit.VisitRepository;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.time.LocalDate;
 import java.util.*;
 import java.lang.*;
@@ -49,6 +52,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class OwnerControllerTests {
 
 	private static final int TEST_OWNER_ID = 1;
+	private static Logger logger = LogManager.getLogger();
 
 	@Autowired
 	private MockMvc mockMvc;
