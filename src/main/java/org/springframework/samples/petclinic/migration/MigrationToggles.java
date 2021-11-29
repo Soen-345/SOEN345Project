c80c3a08b3e67412a0bcd513b1864b89960631e9
eordekian94s <68568405+eordekian94s@users.noreply.github.com>
2021-11-29 00:05:19 -0500

    trying to build

--- src/main/java/org/springframework/samples/petclinic/migration/MigrationToggles.java
+++ src/main/java/org/springframework/samples/petclinic/migration/MigrationToggles.java
@@ -2,7 +2,7 @@ package org.springframework.samples.petclinic.migration;
 
 public class MigrationToggles {
 
-    public static boolean isSQLiteEnabled = false;
+    public static boolean isSQLiteEnabled =  false;
     public static boolean isH2Enabled = true;
     public static boolean isUnderTest = true;
     public static boolean isShadowReadEnabled = true;
